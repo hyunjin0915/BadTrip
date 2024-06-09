ffb9a8257352451abcb05ec4bb8543ccc02b34d2
Toothless <hyunjinney@naver.com>
2024-06-09 22:59:32 +0900

    [Fix] 환각효과 수정
    - 환각 효과 Fade in, out 되게

--- BadTrip/Assets/Scripts/PostProcessingCtrl/PostProcCtrl.cs
+++ BadTrip/Assets/Scripts/PostProcessingCtrl/PostProcCtrl.cs
@@ -8,6 +8,8 @@ using UnityEngine.Rendering;
 
 public class PostProcCtrl : MonoBehaviour
 {
+    bool isPostON = false;
+
         // 환각 작용 관련 변수
     public Cinemachine.PostFX.CinemachineVolumeSettings volumeSettings;
     private ChromaticAberration chromaticAberration;
@@ -28,31 +30,60 @@ public class PostProcCtrl : MonoBehaviour
 
     public void OnHallucinationEffect() // 환각 효과 on
     {
+        isPostON = true;
+
         chromaticAberration.intensity.overrideState = true;
         filmGrain.intensity.overrideState = true;
         lensDistortion.intensity.overrideState = true;
         depthOfField.mode.overrideState = true;
         StartCoroutine(FadeIn());
-        Debug.Log("환각 효과 ON");
     }
     public void OffHallucinationEffect() // 환각 효과 off
     {
+        isPostON = false;
         chromaticAberration.intensity.overrideState = false;
         filmGrain.intensity.overrideState = false;
         lensDistortion.intensity.overrideState = false;
         depthOfField.mode.overrideState = false;
-        Debug.Log("환각 효과 OFF");
     }
 
-    private IEnumerator FadeIn()
+    private IEnumerator RepeatFade()
+    {
+        StartCoroutine(FadeIn());
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(FadeOut());
+        yield return new WaitForSeconds(2f);
+    }
+    public IEnumerator FadeIn()
     {
         float f = 0;
-        while(f==1)
+
+        while(f<=1)
         {
             f += 0.01f;
             chromaticAberration.intensity.value = f;
             filmGrain.intensity.value = f;
-            lensDistortion.intensity.value = f;
+            if(f<=0.6)
+            {
+              lensDistortion.intensity.value = f;
+            }
+            //depthOfField.intensity.value = f;
+            yield return null;
+        }
+    }
+    public IEnumerator FadeOut()
+    {
+        float f = 1;
+
+        while(f>0)
+        {
+            f -= 0.01f;
+            chromaticAberration.intensity.value = f;
+            filmGrain.intensity.value = f;
+            if(f<=0.6)
+            {
+              lensDistortion.intensity.value = f;
+            }
             //depthOfField.intensity.value = f;
             yield return null;
         }

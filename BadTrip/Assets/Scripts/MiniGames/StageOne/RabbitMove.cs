ff95f13683b84043249aaaa27d0d6c2398712406
Toothless <hyunjinney@naver.com>
2024-09-25 23:55:01 +0900

    feat. 이미지 적용

--- BadTrip/Assets/Scripts/MiniGames/StageOne/RabbitMove.cs
+++ BadTrip/Assets/Scripts/MiniGames/StageOne/RabbitMove.cs
@@ -4,40 +4,67 @@ using UnityEngine;
 
 public class RabbitMove : MonoBehaviour
 {
-    private SpriteRenderer rabbitSR;
+    [SerializeField]
+     private float rightMax = 5f;	//좌로 이동가능한 최대값.
+     [SerializeField]
+    private float leftMax = -5f;	//우로 이동가능한 최대값.
+    private float rightMax_Apply;
+    private float leftMax_Apply;
 
-    Vector2 pos;
+    private Vector2 currentPosition;	//현재 위치(x)를 저장할 변수.
+    private float currentScaleX;		//현재 스케일(x)를 저장할 변수.
+    private float currentScaleY;
+
+    private float direction = 1.0f;	//방향.
     [SerializeField]
-    float maxMoving = 2.0f;
-    [SerializeField]
-    float speed = 3.0f;
-    float flippp;
-    bool isFlip = false;
+    private float velocity = 2.0f;	//속도.
+
     void Start()
     {
-        rabbitSR = GetComponent<SpriteRenderer>();
-        pos = transform.position;
+        currentPosition = transform.position; //현재 위치의 x값 저장.
+        currentScaleX = transform.localScale.x;	//현재 스케일의 x값 저장.
+        currentScaleY = transform.localScale.y;	//현재 스케일의 x값 저장.
+
+        rightMax_Apply = currentPosition.x+rightMax;
+        leftMax_Apply = currentPosition.x + leftMax;
     }
-    // Update is called once per frame
+
     void Update()
     {
-        MovingRabbit();
+        Move();
     }
-    void MovingRabbit()
-    {
-        Vector2 v = pos;
-        flippp = maxMoving * Mathf.Sin(Time.time * speed);
-        v.x += flippp;
-        transform.position = v;
 
-        if(flippp>0) 
+    void Move()
+    {
+        currentPosition.x += Time.deltaTime * direction * velocity;
+
+        if (currentPosition.x >= rightMax_Apply)		//현재 위치가 rightMax보다 크거나 같다면
         {
-            isFlip = false;
+            currentPosition.x = rightMax_Apply;			//현재 위치는 rightMax로 설정하고
+            LeftTurn();					//LeftTurn을 실행한다.
         }
-        else
+
+        else if (currentPosition.x <= leftMax_Apply)		//현재 위치가 leftMax보다 크거나 같다면
         {
-            isFlip = true;
+            currentPosition.x = leftMax_Apply;			//현재 위치는 leftMax로 설정하고
+            RightTurn();				//RightTurn을 실행한다.
         }
-        rabbitSR.flipX = isFlip;
+        transform.position = currentPosition;
+        //transform.position = new Vector2(currentPosition.x, currentPosition.y);
     }
+
+    private void LeftTurn()
+    {
+        direction = -1f;				//방향을 왼쪽으로 설정
+        //currentScaleX에 -1을 곱한 값과 currentScaleY로 새로운 벡터를 생성해 localScale에 할당..
+        transform.localScale = new Vector2(currentScaleX * -1, currentScaleY);
+    }
+
+    private void RightTurn()
+    {
+        direction = 1f;					//방향을 오른쪽으로 설정(원래 방향과 동일).
+        transform.localScale = new Vector2(currentScaleX, currentScaleY);
+    }
+
+
 }

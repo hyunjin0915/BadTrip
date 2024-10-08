d278622049c956f7ad1599afcdc044237474e3d4
Toothless <hyunjinney@naver.com>
2024-10-08 16:25:23 +0900

    fix. 몬스터 프리팹 수정
    - 이전 몬스터 이동 스크립트 삭제 및 프리팹들 수정

---
+++ BadTrip/Assets/Scripts/MiniGames/Dream/MudMonMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudMonMove : MonoBehaviour
+{
+    /*
+     랜덤 스폰, 스폰 순간부터 플레이어 위치 값 얻어 해당 방향으로 이동
+     플레이어가 데미지 입힐 수  X
+    일정 간격으로 속도를 높여 플레이어 대쉬
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

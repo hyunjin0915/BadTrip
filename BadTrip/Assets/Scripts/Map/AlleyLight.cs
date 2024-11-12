435aaeac7ad63e3f63f477324badca695aca318a
Yeon09a <68226341+Yeon09a@users.noreply.github.com>
2024-11-13 02:42:38 +0900

    [perf] 스토리 연결 및 StageOne Player 디자인 변경, 드림캐쳐 연출 추가

--- BadTrip/Assets/Scripts/Map/AlleyLight.cs
+++ BadTrip/Assets/Scripts/Map/AlleyLight.cs
@@ -15,11 +15,16 @@ public class AlleyLight : MonoBehaviour
     void Start()
     {
         // 퀘스트 번호에 따라 달라짐.
-        
-        /*if (conversationEventManager.GetCurQuestNum )
-        {
 
-        }*/
+        if (conversationEventManager.GetCurQuestNum() <= 14)
+        {
+            alleySP.sprite = night;
+            lights.SetActive(true);
+        } else
+        {
+            alleySP.sprite = day;
+            lights.SetActive(false);
+        }
     }
 
     

74a546f1d8f277461771c7ce996eb90b5301d4da
Toothless <hyunjinney@naver.com>
2024-11-14 14:51:00 +0900

    feat. dream 몬스터 죽음
    - 뒤집어지고 사라지도록 수정 등등

--- BadTrip/Assets/Scripts/MiniGames/Dream/PlayerHDTrigger.cs
+++ BadTrip/Assets/Scripts/MiniGames/Dream/PlayerHDTrigger.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHDTrigger : MonoBehaviour
@@ -5,12 +7,16 @@ public class PlayerHDTrigger : MonoBehaviour
     [SerializeField, Tooltip("How much should the player's health Decrease")]
     private float PlayerDamage = 10f;
     private Rigidbody2D myRigid;
+    private PolygonCollider2D myCollider;
+    SpriteRenderer spriteRenderer;
 
     [SerializeField]
     private PlayerHPSO Player_healthManager;
     void Start()
     {
         myRigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<PolygonCollider2D>();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,8 +24,10 @@ public class PlayerHDTrigger : MonoBehaviour
         {
             if(collision.rigidbody.velocity.y<0 && transform.position.y < collision.transform.position.y)
             {
-                Debug.Log("밟기!");
-                transform.gameObject.SetActive(false);
+                collision.rigidbody.AddForce(Vector2.up*100, ForceMode2D.Impulse);
+                OnDamaged();
+                //Debug.Log("밟기!");
+                //transform.gameObject.SetActive(false);
             }
             else
             {
@@ -27,5 +35,18 @@ public class PlayerHDTrigger : MonoBehaviour
             }
             
         }
+    }
+
+    private void OnDamaged()
+    {
+        if(myCollider!=null)
+            myCollider.isTrigger = true;
+        spriteRenderer.flipY = true;
+        myRigid.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+        Invoke("DeActive", 0.5f);
+    }
+    private void DeActive()
+    {
+        transform.gameObject.SetActive(false);
     }
 }
